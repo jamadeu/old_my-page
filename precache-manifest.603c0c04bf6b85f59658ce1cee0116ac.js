4753866cbcc0750d87c447783a3970ecd77ca6d1
Jean Amadeu <jean.amadeu2@gmail.com>
2020-05-17 14:32:54 -0300

    Updates

---
+++ precache-manifest.603c0c04bf6b85f59658ce1cee0116ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d16e2b653902d481640fb23a5575b9",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "7ce616b2de2f3fb2724e",
+    "url": "/my-page/static/js/2.a509ecea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-page/static/js/2.a509ecea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef64839ab76fba67b612",
+    "url": "/my-page/static/js/main.d0523325.chunk.js"
+  },
+  {
+    "revision": "0cbd4908dcc63d6e7f5a",
+    "url": "/my-page/static/js/runtime-main.7fa03308.js"
+  }
+]);
